1dd98c11504ddc0b8d145f11ef2aafac99232ac5
YousefHassan52 <yousef52hm@gmail.com>
2024-08-16 18:35:15 +0300

    deal with timestamp as carbon object

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -29,7 +29,7 @@
           <th scope="row">{{$post->id}}</th>
           <td>{{$post->title}}</td>
           <td>{{$post->user->name}}</td>
-          <td>{{$post->created_at}}</td>
+          <td>{{$post->created_at->addYears(15)->format("Y-m-d")}}</td>
           <td>
             <a href="{{route('posts.show',$post->id)}}"  class="btn btn-info">View</a>{{--lw 3ayez te pass more than url parameter put them in associative array and the key must be the same as the url parameter in route (web.php file) --}}
             <a href="{{route('posts.edit',$post->id)}}" class="btn btn-primary">Edit</a>
